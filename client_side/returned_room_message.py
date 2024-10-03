7d97c2ec4a9a2457804d5ca7c1807fcec5ea8347
Khai <ltri0527@uni.sydney.edu.au>
2024-10-03 07:31:49 +0700

    revert

---
+++ client_side/returned_room_message.py
@@ -0,0 +1,38 @@
+from client_side.returned_authentication_message import check_bad_auth
+
+def handle_returned_create(message, room_name):
+    bad_auth_msg = check_bad_auth(message)
+    if bad_auth_msg:
+        return bad_auth_msg
+    status = message.split(":")[2]
+    if status == "0":
+        return f"Successfully created room name {room_name}"
+    elif status == "1":
+        return f"Error: Room {room_name} is invalid"
+    elif status == "2":
+        return f"Error: Room {room_name} already exists"
+    elif status == "3":
+        return f"Error: Server already contains a maximum of 256 rooms"
+
+def handle_returned_join(message, room_name, mode):
+    bad_auth_msg = check_bad_auth(message)
+    if bad_auth_msg:
+        return bad_auth_msg
+    status = message.split(":")[2]
+    if status == "0":
+        return f"Successfully joined room {room_name} as a {mode}"
+    elif status == "1":
+        return f"Error: No room named {room_name}\n"
+    elif status == "2":
+        return f"Error: The room {room_name}  already has 2 players"
+
+def handle_returned_room_list(message, mode):
+    bad_auth_msg = check_bad_auth(message)
+    if bad_auth_msg:
+        return bad_auth_msg
+    status = message.split(":")[2]
+    room_list = message.split(":")[3]
+    if status == "0":
+        return f"Room available to join as {mode}: {room_list}"
+    elif status == "1":
+        return "ClientError: Please input a valid mode"
