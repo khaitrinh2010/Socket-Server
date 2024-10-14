478c7c8139f4ea64e232618cbda1d044c548b25e
Khai <ltri0527@uni.sydney.edu.au>
2024-10-15 06:22:27 +0700

    test

--- server.py
+++ server.py
@@ -37,11 +37,11 @@ def handle_client_message(message, path, sock:socket):
         if components[0] == "LOGIN":
             SOCKET_TO_USER[sock] = username
     else:
-        if components[0] in ["PLACE", "FORFEIT"] and not USERS[SOCKET_TO_USER[sock]].get_room():
-            sock.send("NOROOM".encode('ascii'))
-            return
         if not SOCKET_TO_USER.get(sock):
             sock.send("BADAUTH".encode('ascii'))
+            return
+        if components[0] in ["PLACE", "FORFEIT"] and not USERS[SOCKET_TO_USER[sock]].get_room():
+            sock.send("NOROOM".encode('ascii'))
             return
         if components[0] in ["ROOMLIST", "JOIN", "CREATE"]:
             handle_room_message(message, ROOMS, USERS, SOCKET_TO_USER[sock], SOCKET_TO_USER, sock)
