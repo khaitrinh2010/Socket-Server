f513cf493aa8b552c74a90676e4aa15bbf4b2409
Khai <ltri0527@uni.sydney.edu.au>
2024-10-17 08:19:06 +0700

    test

--- game_handler/game_management.py
+++ game_handler/game_management.py
@@ -101,7 +101,7 @@ def handle_place(message, username, all_users, room_name, all_rooms):
 
     if not handle_game_end_and_forfeit(message, username, all_users, room_name, all_rooms):
         handle_board_status(all_rooms[room_name])
-        if room.get_cache() and room.get_cache()[0][3] != room.get_current_turn():
+        if room.get_cache() and room.get_cache()[0][3] == room.get_current_turn():
             handle_place(f"PLACE:{room.get_cache()[0][1]}:{room.get_cache()[0][2]}.", room.get_current_turn().get_username(), all_users, room_name, all_rooms)
 
 def handle_game_end_and_forfeit(message, username, all_users, room_name, all_rooms):
