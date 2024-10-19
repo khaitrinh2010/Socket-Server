50e8c39d5f674fc111cf423833fad6f65296d555
Khai <ltri0527@uni.sydney.edu.au>
2024-10-19 11:18:45 +0700

    test

--- client_side/returned_game_message.py
+++ client_side/returned_game_message.py
@@ -1,12 +1,25 @@
+import sys
+
+
 def handle_return_in_progress(message):
     message = message.split(":")
     player1, player2 = message[1], message[2]
     return f"Match between {player1} and {player2} is in progress, it is currently {player1}'s turn."
 
 def handle_return_board_status(message):
-    message = message.split(":")
-    board = message[1]
-    return f"{board}"
+    message = message.strip().split(":")
+    board = message[1].replace('1', 'X').replace('2', 'O').replace('0', ' ')
+    row_separator = "----------"
+    column_separator = "|"
+
+    formatted_board = row_separator + "\n"
+    for i in range(0, len(board), 3):
+        if i == 9:
+            break
+        row = f" {board[i]} {column_separator} {board[i+1]} {column_separator} {board[i+2]} "
+        formatted_board += row + "\n" + row_separator + "\n"
+
+    return formatted_board
 
 def handle_return_game_end(message, is_player, username):
     message = message.split(":")
